d5b77325768eb281d14fa722afe5072c57d2f66b
Bushra1995 <bushra95ga@gmail.com>
2023-03-19 21:28:39 +0300

    loops

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 alert("Welcome to Caffe-Z")
-let name = prompt("What's your name?");
-let gender = prompt("what is your Gender male/female ?");
-if (gender=="female"){
-    alert("Welcome MS " +name+"!")
+let userInfo = [];
+let user = prompt("What's your name?");
+let gender = "";
+
+
+// if (gender=="female"){
+//     alert("Welcome MS " +name+"!")
+// }
+// else if (gender=="male"){
+//     alert("Welcome MR" +name+"!")
+// }
+// else {
+//     alert("Welcome " +name+"!")
+// }
+
+
+while (gender !== "male" && gender !== "female") {
+    gender = prompt("Please enter a gender (male, female):");
 }
-else if (gender=="male"){
-    alert("Welcome MR" +name+"!")
-}
-else {
-    alert("Welcome " +name+"!")
-}
+
 let drinkType = prompt("Would you like a hot or cold drink?☕🍷");
 let drinkName = prompt("What kind of drink would you like?");
 
 alert("Your " + drinkType + " " + drinkName + " is being prepared!☕");
 
-console.log(name + " ordered a " + drinkType + " " + drinkName + ".");
+userInfo.push(user, gender, drinkType, drinkName)
+
+for (let i = 0; i < userInfo.length; i++) {
+    console.log(userInfo[i])
+}
